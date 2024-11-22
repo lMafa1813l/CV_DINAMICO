0936fc53481e9e210231b4b58d7983651292dbe0
Fabián Esteban Medina Grados <mafamedina1813@gmail.com>
2024-11-21 19:36:08 -0500

    Xamarin_form_Dinamico

---
+++ Forms/Forms/CvPreviewPage.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Forms
+{
+    public partial class CvPreviewPage : ContentPage
+    {
+        public CvPreviewPage(string nombre, string correo, string telefono, string direccion, string perfil,
+                             StackLayout formacion, StackLayout experienciaLaboral, StackLayout habilidades)
+        {
+            InitializeComponent();
+
+         
+            NombreLabel.Text = $"Nombre: {nombre}";
+            CorreoLabel.Text = $"Correo Electrónico: {correo}";
+            TelefonoLabel.Text = $"Teléfono: {telefono}";
+            DireccionLabel.Text = $"Dirección: {direccion}";
+            PerfilLabel.Text = perfil;
+
+            FormacionLayout.Children.Add(CloneStackLayout(formacion));
+            ExperienciaLaboralLayout.Children.Add(CloneStackLayout(experienciaLaboral));
+            HabilidadesLayout.Children.Add(CloneStackLayout(habilidades));
+        }
+
+        private StackLayout CloneStackLayout(StackLayout original)
+        {
+            var clone = new StackLayout();
+            foreach (var child in original.Children)
+            {
+                if (child is Label label)
+                {
+                    clone.Children.Add(new Label { Text = label.Text, FontSize = label.FontSize, Margin = label.Margin });
+                }
+                else if (child is Entry entry)
+                {
+                    clone.Children.Add(new Entry { Text = entry.Text, Placeholder = entry.Placeholder, FontSize = entry.FontSize });
+                }
+                else if (child is StackLayout stackLayout)
+                {
+                    clone.Children.Add(CloneStackLayout(stackLayout));
+                }
+                
+            }
+            return clone;
+        }
+    }
+}
---
+++ Forms/Forms/MainPage.xaml.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Forms
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+
+        private void OnAddEducationClicked(object sender, EventArgs e)
+        {
+            var educationStack = new StackLayout
+            {
+                Children =
+                {
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label { Text = "Grado:", WidthRequest = 120, FontSize = 18 },
+                            new Entry { Placeholder = "Ingrese su grado", HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label { Text = "Institución:", WidthRequest = 120, FontSize = 18 },
+                            new Entry { Placeholder = "Ingrese su institución", HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label { Text = "Año:", WidthRequest = 120, FontSize = 18 },
+                            new Entry { Placeholder = "Ingrese el año de graduación", Keyboard = Keyboard.Numeric, HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                        }
+                    }
+                }
+            };
+
+            SeccionFormacion.Children.Add(educationStack);
+        }
+
+        private void OnAddWorkExperienceClicked(object sender, EventArgs e)
+        {
+            var workExperienceStack = new StackLayout
+            {
+                Children =
+                {
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label { Text = "Empresa:", WidthRequest = 120, FontSize = 18 },
+                            new Entry { Placeholder = "Ingrese el nombre de la empresa", HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label { Text = "Puesto:", WidthRequest = 120, FontSize = 18 },
+                            new Entry { Placeholder = "Ingrese su puesto", HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                        }
+                    },
+                    new StackLayout
+                    {
+                        Orientation = StackOrientation.Horizontal,
+                        Children =
+                        {
+                            new Label { Text = "Años:", WidthRequest = 120, FontSize = 18 },
+                            new Entry { Placeholder = "Ingrese los años trabajados", HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                        }
+                    }
+                }
+            };
+
+            SeccionExperienciaLaboral.Children.Add(workExperienceStack);
+        }
+
+        private void OnAddSkillClicked(object sender, EventArgs e)
+        {
+            var skillStack = new StackLayout
+            {
+                Orientation = StackOrientation.Horizontal,
+                Children =
+                {
+                    new CheckBox(),
+                    new Entry { Placeholder = "Ingrese habilidad", HorizontalOptions = LayoutOptions.FillAndExpand, FontSize = 16 }
+                }
+            };
+
+            SeccionHabilidades.Children.Add(skillStack);
+        }
+
+
+        private void OnSubmitClicked(object sender, EventArgs e)
+        {
+            string nombre = NombreEntry.Text;
+            string correo = CorreoEntry.Text;
+            string telefono = TelefonoEntry.Text;
+            string direccion = DireccionEntry.Text;
+            string perfil = PerfilEditor.Text;
+
+            var formacion = SeccionFormacion;
+            var experienciaLaboral = SeccionExperienciaLaboral;
+            var habilidades = SeccionHabilidades;
+
+            var cvPreviewPage = new CvPreviewPage(nombre, correo, telefono, direccion, perfil, formacion, experienciaLaboral, habilidades);
+            Navigation.PushAsync(cvPreviewPage);
+        }
+    }
+}
